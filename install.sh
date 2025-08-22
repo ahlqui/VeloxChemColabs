d6549d8468626ac78f7e17d4312ac274e8af00ab
Mårten Ahlquist <156903267+ahlqui@users.noreply.github.com>
2025-08-22 15:49:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+!/bin/bash
 python sys.path.append('/usr/local/lib/python3.12/site-packages/')
 mamba install -y -c conda-forge -c veloxchem veloxchem xtb-python dftd4-python py3Dmol MDAnalysis openmm matplotlib python=3.12
 pip install rdkit
