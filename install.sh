cefee5cfc90ecd42b41d0c44496305bc4592b2b9
Mårten Ahlquist <156903267+ahlqui@users.noreply.github.com>
2025-08-22 15:45:01 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+python sys.path.append('/usr/local/lib/python3.12/site-packages/')
+mamba install -y -c conda-forge -c veloxchem veloxchem xtb-python dftd4-python py3Dmol MDAnalysis openmm matplotlib python=3.12
+pip install rdkit
+python import veloxchem as vlx
