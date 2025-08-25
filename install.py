fbfc2eea8bb34dc4d5324e3d60ae6a25d097838d
Mårten Ahlquist <156903267+ahlqui@users.noreply.github.com>
2025-08-25 12:35:42 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,12 @@
 import sys
 import os
 
+python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
+
 os.system("wget -qnc https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh")
 os.system("bash Miniforge3-Linux-x86_64.sh -bfp /usr/local")
 os.system("mamba config --set auto_update_conda false")
-sys.path.append('/usr/local/lib/python3.12/site-packages/')
-os.system("mamba install -y -c conda-forge -c veloxchem veloxchem xtb-python dftd4-python py3Dmol MDAnalysis openmm matplotlib python=3.12")
+site_packages_path = f"/usr/local/lib/python{python_version}/site-packages/"
+os.environ['PYTHONPATH'] = site_packages_path + ':' + os.environ.get('PYTHONPATH', '')
+os.system(f"mamba install -y -c conda-forge -c veloxchem veloxchem xtb-python dftd4-python py3Dmol MDAnalysis openmm matplotlib python={python_version}")
 os.system("pip install rdkit")
-import veloxchem as vlx
