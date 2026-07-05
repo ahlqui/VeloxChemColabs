742c41d4fcf1f48a741efc5d943db3073baa8d7c
Mårten Ahlquist <156903267+ahlqui@users.noreply.github.com>
2026-07-05 16:06:11 +0200

    Update OpenMM version in install script

--- install.py
+++ install.py
@@ -8,5 +8,5 @@ os.system("bash Miniforge3-Linux-x86_64.sh -bfp /usr/local")
 os.system("mamba config --set auto_update_conda false")
 site_packages_path = f"/usr/local/lib/python{python_version}/site-packages/"
 os.environ['PYTHONPATH'] = site_packages_path + ':' + os.environ.get('PYTHONPATH', '')
-os.system(f"mamba install -y -c conda-forge -c veloxchem veloxchem xtb-python dftd4-python py3Dmol MDAnalysis openmm matplotlib python={python_version}")
+os.system(f"mamba install -y -c conda-forge -c veloxchem veloxchem xtb-python dftd4-python py3Dmol MDAnalysis openmm=8.2 matplotlib python={python_version}")
 os.system("pip install rdkit")
